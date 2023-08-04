5be9d6f55589a90aeb6f1d5d6b2a5d57e9504cb3
Brenokardoso <brenopositivo@hotmail.com>
2023-08-04 11:56:53 -0300

    Método get passado por url

--- Pythorando/Aula_de_django/Autenticacao/views.py
+++ Pythorando/Aula_de_django/Autenticacao/views.py
@@ -4,7 +4,14 @@ from Aula_de_django.settings import BASE_DIR
 
 
 def cadastro(request):    
-    return render(request,'cadastro/index.html')  #aqui no caso o dicionario trabalha se auto referenciando,igual um (self ou cls)
+    
+    nome = request.GET.get('nome')
+    sobrenome = request.GET.get('sobrenome')
+    idade = request.GET.get('idade')
+
+    print("nome =",nome,"\nsobrenome =",sobrenome,"\nidade =",idade)
+
+    return  (request,'cadastro/index.html',{'nome':nome,'sobrenome':sobrenome,'idade':idade})  #aqui no caso o dicionario trabalha se auto referenciando,igual um (self ou cls)
 
 
 def home(request):
